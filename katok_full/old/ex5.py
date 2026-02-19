1f3ceb3a38169514f779438e17401237e8bde137
parse du <duparse@gmail.com>
2026-02-19 06:39:36 +0300

    Первый коммит: проект после завершения главы 9

--- katok_full/old/ex5.py
+++
@@ -1,8 +0,0 @@
-import nltk
-from nltk.stem import WordNetLemmatizer
-tokenized_words = ['go', 'went', 'gone', 'am', 'are', 'is', 'was', 'were']
-lemmatizer = WordNetLemmatizer()
-nltk.download("wordnet")
-
-print([lemmatizer.lemmatize(word, pos='v') for word in tokenized_words])
-
